adc4a9ef9cd5330af8c80c716de68a5f4be43a5e
Mr-Kesh <123339974+Mr-Kesh@users.noreply.github.com>
2024-12-09 12:54:23 -0700

    Update and rename FinalProject.js to FinalProjectATLS.js

--- FinalProjectATLS.js
+++ FinalProjectATLS.js
@@ -2,7 +2,6 @@ function closeModal() {
     document.getElementById('click-toggle').checked = false;
 }
 
-
 document.addEventListener("DOMContentLoaded", () => {
     const hamburgerIcon = document.getElementById("hamburger-icon");
     const navMenu = document.getElementById("nav-menu");
